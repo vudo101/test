718adb62c5aba0ed6994971494beb8547613c79e
vudo101 <128112073+vudo101@users.noreply.github.com>
2023-07-12 14:56:11 +0700

    Update sns.tf

--- sns.tf
+++ sns.tf
@@ -4,7 +4,7 @@ resource "aws_sns_topic" "system-alert" {
 
 resource "aws_sns_topic_subscription" "email-target" {
 
-  for_each  = toset(["abc@gmail.com", "nat@gmail.com"])
+  for_each  = toset(["abcd@gmail.com", "nat@gmail.com"])
   topic_arn = aws_sns_topic.system-alert.arn
   protocol  = "email"
   endpoint  = each.value
